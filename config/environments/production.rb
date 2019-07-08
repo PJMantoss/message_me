6807270cee8f3417ac2717e935d108f6a9405371
PJ Mantoss <joelptoss@gmail>
2019-07-08 17:07:00 +0100

    Add sign up form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
+  https://mantoss-message-me.herokuapp.com
   # Settings specified here will take precedence over those in config/application.rb.
-
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
